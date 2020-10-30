c1c1fa5a72cf07f3fd37bcc84bbc5ccb0193913e
feliciavonbraun <feliciavonbraun@hotmail.com>
2020-10-30 16:13:08 +0100

    small changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//DEFINERA ALLA SCENER I SPELET
+/** Set the startscene to 0 and let it change */
 let currentScene = 0;
 
 /** All the scenes */
@@ -14,7 +14,7 @@ const scenes = [
         nextScene: [3,2]
     },
     {   //SCENE 2 KEEP CALM
-        description: "You put on the oxygen mask. The staff hands out parachutes. Are you the first or last one who jumpes?",
+        description: "You put on the oxygen mask. The staff hands out parachutes. Are you the first or last one who jumps?",
         choices: ["LAST", "FIRST"],
         nextScene: [7,4]
     },
@@ -29,18 +29,18 @@ const scenes = [
         nextScene: [5,6]
     },
     {   //SCENE 5 KNOW HOW TO SWIM
-        description: "You swim for as long as you can but it's not enough. Sadly you drown. Do you want to play again?",
+        description: "GAME OVER You swim for as long as you can but it's not enough. Sadly you drown. Do you want to play again?",
         choices: ["YES", "NO"],
         nextScene: [0,8]
     },
     {   //SCENE 6 DONT KNOW HOW TO SWIM
-        description: "GAME OVER. You dont know how to swim and drown. Do you want to play again?",
+        description: "GAME OVER. You don't know how to swim and drown. Do you want to play again?",
         choices: ["YES", "NO"],
         nextScene: [0,8]
     },
     {   //SCENE 7 LAST MÅL
-        description: "Your the last one to jump and you land on Hawaii and live happily ever after. Do you want to play again?",
-        choices: ["YAAS", "NO"],
+        description: "HURRAY You're the last one to jump and you land on Hawaii and live happily ever after. Do you want to play again?",
+        choices: ["YAAS", "NAH"],
         nextScene: [0,8]
     },
     {   //SCENE 8 GOODBYE
@@ -51,16 +51,16 @@ const scenes = [
 ]
 
 
-// SKA BYTA TEXTEN PÅ P OCH KNAPPARNA. och starta vid laddning av sidan
+/** Present paragraf and button options onload of page. */
 window.onload = presentScene;
 
 /** The active secene on webpage. */
 function presentScene() {
-    const textWhoChange = document.getElementById("textWhoChange") //se till att början blir ljusblå och läs felmeddelandet
-    const btn1 = document.getElementById("btn1") // ska det va ' eller " ???
+    const textWhoChange = document.getElementById("textWhoChange")
+    const btn1 = document.getElementById("btn1") 
     const btn2 = document.getElementById("btn2") 
     console.log (btn1, btn2)
-    textWhoChange.innerHTML = scenes[currentScene].description; //decription kommer från scenerna
+    textWhoChange.innerHTML = scenes[currentScene].description;
     
     if (scenes[currentScene].choices.length === 0) {
         btn1.style.display = "none";
@@ -78,22 +78,16 @@ function presentScene() {
     }
 }
 
-// SKA HANTERA VILKET ANVÄNDAREN TRYCKER PÅ
-/** Handles user choise and presents the next scene */
+/** Handles which button the user press and presents the next scene. */
 function handleUserChoice(nextScene) {
     console.log(nextScene);
 
-
     currentScene = nextScene; 
     presentScene(); 
-
-    //skapa en scen som kör om det inte finns några val, game over 
-
 }
 
-//EN FUNKTION SOM LADDAR OM SIDAN NÄR MAN TRYCKER PÅ H1
+/** Reloads page when pressing on h1*/
 function reloadPage(){
     currentScene = 0;
-    presentScene();  
-      
+    presentScene();    
 }
